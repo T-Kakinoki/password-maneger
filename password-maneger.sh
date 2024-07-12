df1f932969e6ab485ed44959bd82db2f6a95ff8f
kakinoki <kknk.courage@gmail.com>
2024-07-13 01:26:34 +0900

    変数Choises、その分岐先の追加

--- password-maneger.sh
+++ password-maneger.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-
 #起動時
-echo パスワードマネージャーへようこそ！
-echo 次の選択肢から入力してください(Add Password/Get Password/Exit)：
+echo "パスワードマネージャーへようこそ！"
+echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+ read choices
 #Add password 入力時
-echo パスワードマネージャーへようこそ！
-echo サービス名を入力してください：
-echo ユーザー名を入力してください：
-echo パスワードを入力してください：
+ case $choices in
+ Add Password)
+  echo "パスワードマネージャーへようこそ！"
+  echo "サービス名を入力してください："
+  echo "ユーザー名を入力してください："
+  echo "パスワードを入力してください："
 # 入力が完了したら
-echo Thank you!
+  echo "パスワードの追加は成功しました。"
+  ;;
 #get password 入力時
-
+ Get Password)
+  echo "サービス名を入力してください："
+## サービス名が保存されていなかった場合
+   echo "そのサービスは登録されていません。"
+## サービス名が保存されていた場合
+   echo "サービス名："
+   echo "ユーザー名："
+   echo "パスワード："
+   ;;
 #Exit入力時
-echo Thank you!
-
-# Add Password/Get Password/Exit 以外が入力された場合
-echo 入力が間違えています。Add Password/Get Password/Exit から入力してください。
+ Exit)
+   echo "Thank you!"
+   ;;
+# Add Password/Get Password/Exit 以外が入力された場合   
+ *)
+   echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+   ;;
+ esac
