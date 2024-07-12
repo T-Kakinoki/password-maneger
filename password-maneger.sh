94a180d270f56316cc19bf37c99b4353e633f102
kakinoki <kknk.courage@gmail.com>
2024-07-13 00:16:41 +0900

    change file name

---
+++ password-maneger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#起動時
+echo パスワードマネージャーへようこそ！
+echo 次の選択肢から入力してください(Add Password/Get Password/Exit)：
+#Add password 入力時
+echo パスワードマネージャーへようこそ！
+echo サービス名を入力してください：
+echo ユーザー名を入力してください：
+echo パスワードを入力してください：
+# 入力が完了したら
+echo Thank you!
+#get password 入力時
+
+#Exit入力時
+echo Thank you!
+
+# Add Password/Get Password/Exit 以外が入力された場合
+echo 入力が間違えています。Add Password/Get Password/Exit から入力してください。
